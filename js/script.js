b619ff728e452159d3bf1a224200e96f704288fd
adhyaksasaptapamungkas <adhyaksasapta19@gmail.com>
2022-02-03 20:34:35 +0700

    merubah portfolio

--- js/script.js
+++ js/script.js
@@ -10,13 +10,4 @@ $(window).scroll(function () {
   $('.jumbotron p').css({
     transform: 'translate(0px, ' + wScroll / 2 + '%)',
   });
-
-  //   portfolio
-  if (wScroll > $('.portfolio').offset().top - 250) {
-    $('.portfolio .thumbnail').each(function (i) {
-      setTimeout(function () {
-        $('.portfolio .thumbnail').eq(i).addClass('muncul');
-      }, 300 * (i + 1));
-    });
-  }
 });
