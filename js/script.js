0e0a401ed6b4ccd7c3d6922fb647bdf7f2146dbc
adhyaksasaptapamungkas <adhyaksasapta19@gmail.com>
2022-02-03 18:19:53 +0700

    mengubah parallax

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
   $('.jumbotron img').css({
-    transform: 'translate(0px, ' + wScroll / 4 + '%)',
+    transform: 'translate(0px, ' + wScroll / 6 + '%)',
   });
   $('.jumbotron h1').css({
-    transform: `translate(0px, ${wScroll / 2}%)`,
+    transform: 'translate(0px, ' + wScroll / 4 + '%)',
   });
   $('.jumbotron p').css({
-    transform: 'translate(0px, ' + wScroll / 1.2 + '%)',
+    transform: 'translate(0px, ' + wScroll / 2 + '%)',
   });
 
   //   portfolio
